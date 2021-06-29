f363ed646578c530a6ae071e2fe7d6164910d899
Brewal Derouet <brewal_derouet@hotmail.fr>
2021-06-29 22:22:44 +0200

    feat: Typing (#6)
    * chore: Setup package.json
    
    * chore: Add .gitignore
    
    * chore: Setup Yarn
    
    * chore: Add .editorconfig
    
    * chore: Config ESLint
    
    * chore: Config Prettier
    
    * chore: Add tsconfig.json
    
    * chore: Add package scripts
    
    * feat: Typing (#2)
    
    * ci: Add Github Action workflows (#3)
    
    * fix: package version start
    
    * fix(ci): typo in release-please.yml

---
+++ src/rest/payment.ts
@@ -0,0 +1,66 @@
+import { APIPartialPayment, APIPayment } from '../payloads';
+
+/**
+ * https://github.com/pi-apps/pi-platform-docs/blob/master/platform_API.md#get-a-payment
+ */
+export type RESTGetPaymentResult = APIPayment;
+
+/**
+ * Undocumented route (POST /payments)
+ */
+export type RESTPostCreatePaymentJSONBody = APIPartialPayment;
+
+/**
+ * Undocumented route (POST /payments)
+ */
+export type RESTPostCreatePaymentResult = APIPayment;
+
+/**
+ * https://github.com/pi-apps/pi-platform-docs/blob/master/platform_API.md#approve-a-payment
+ */
+export type RESTPostApprovePaymentResult = APIPayment;
+
+/**
+ * Undocumented route (POST /payments/:payment_id/verify_transaction)
+ */
+export interface RESTPostVerifyTransactionJSONBody {
+	/**
+	 * The id of the blockchain transaction
+	 */
+	txid: string;
+}
+
+/**
+ * Undocumented route (POST /payments/:payment_id/verify_transaction)
+ */
+export type RESTPostVerifyTransactionResult = APIPayment;
+
+/**
+ * https://github.com/pi-apps/pi-platform-docs/blob/master/platform_API.md#complete-a-payment
+ */
+export interface RESTPostCompletePaymentJSONBody {
+	/**
+	 * The id of the blockchain transaction
+	 */
+	txid: string;
+}
+
+/**
+ * https://github.com/pi-apps/pi-platform-docs/blob/master/platform_API.md#complete-a-payment
+ */
+export type RESTPostCompletePaymentResult = APIPayment;
+
+/**
+ * Undocumented route (GET /payments/recent)
+ */
+export interface RESTGetRecentPaymentResult {
+	/**
+	 * Whether or not there is a recent payment
+	 */
+	exists: boolean;
+
+	/**
+	 * The recent payment if there is one
+	 */
+	payment: null | APIPayment;
+}
