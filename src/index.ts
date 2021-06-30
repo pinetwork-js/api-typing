45f7e6afe8e9102e4be189ff472d1337b1b2c1f9
Brewal Derouet <brewal_derouet@hotmail.fr>
2021-07-01 00:42:50 +0200

    feat: group export (#12)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 export * from './payloads';
+export * as payloads from './payloads';
+
 export * from './rest';
+export * as rest from './rest';
+
 export * from './routes';
+export * as routes from './routes';
