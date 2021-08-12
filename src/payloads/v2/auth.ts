0bfd2c0da9c3c187ff2c11380cb1c4fc7b5d7c17
B-Derouet <brewal_derouet@hotmail.fr>
2021-08-12 03:31:11 +0200

    feat: new APIUser type with scope system

--- src/payloads/v2/auth.ts
+++ src/payloads/v2/auth.ts
@@ -5,9 +5,9 @@ type APIUserRoles = 'email_verified' | 'core_team' | 'mega_mod' | 'moderator';
  */
 export interface APIUser {
 	/**
-	 * The username of the user
+	 * The username of the user if `username` scope is specified.
 	 */
-	username: string;
+	username?: string;
 
 	/**
 	 * The UID of the user
@@ -15,7 +15,7 @@ export interface APIUser {
 	uid: string;
 
 	/**
-	 * The roles of the user
+	 * The roles of the user if `roles` scope is specified.
 	 */
-	roles: APIUserRoles[];
+	roles?: APIUserRoles[];
 }
